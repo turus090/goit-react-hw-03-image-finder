1791bd8de597f6d6e3f63639ebdb6d8e38b624ac
Sergii Tatianchenko <taurus090@gmail.com>
2023-06-30 09:51:56 +0300

    fixed

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,79 +1,93 @@
-import axios from "axios";
-import ImageGallery from "./imageGallery/ImageGallery";
-import Searchbar from "./searchbar/Searchbar";
-import { Component } from "react";
-import Button from "./button/Button";
-import Modal from "./modal/Modal";
+import axios from 'axios';
+import ImageGallery from './imageGallery/ImageGallery';
+import Searchbar from './searchbar/Searchbar';
+import { Component } from 'react';
+import Button from './button/Button';
+import Modal from './modal/Modal';
 
 export class App extends Component {
-  state = { 
-    images:[],
-    searchText: "",
+  state = {
+    images: [],
+    searchText: '',
     page: 1,
     countOnPage: 20,
     modalOpen: false,
     imgModal: null,
-  }
-  apiKey = "36205936-cd8fb584a14544fbe3836796c"
-  baseUrl ="https://pixabay.com/api/"
-  render (){
-    const updateSearch = (newSearch) =>{
+  };
+  apiKey = '36205936-cd8fb584a14544fbe3836796c';
+  baseUrl = 'https://pixabay.com/api/';
+  render() {
+    const updateSearch = newSearch => {
       this.setState({
-        ...this.state, 
-        searchText : newSearch
-      })
-    }
+        ...this.state,
+        searchText: newSearch,
+      });
+    };
     const searchAPI = () => {
-      console.log('click')
-        axios.get(`${this.baseUrl}/?q=${this.state.searchText}&key=${this.apiKey}&image_type=photo&orientation=horizontal&per_page=${this.state.countOnPage}&page=${this.state.page}`)
-          .then(res=>{
-            console.log(res)
-            this.setState({
-              ...this.state,
-              images: [
-                ...res.data.hits
-              ]
-            })
-          })
-    }
-    const handleOpenModal = (img) => {
+      console.log('click');
+      axios
+        .get(
+          `${this.baseUrl}/?q=${this.state.searchText}&key=${this.apiKey}&image_type=photo&orientation=horizontal&per_page=${this.state.countOnPage}&page=${this.state.page}`
+        )
+        .then(res => {
+          console.log(res);
+          this.setState({
+            ...this.state,
+            images: [...res.data.hits],
+          });
+        });
+    };
+    const handleOpenModal = img => {
       this.setState({
         ...this.state,
         imgModal: img,
-        modalOpen: true
-      })
-    }
+        modalOpen: true,
+      });
+    };
     const handleCloseModal = () => {
       this.setState({
-      ...this.state,
-      imgModal: null,
-      modalOpen: false          
-      })
-    }
+        ...this.state,
+        imgModal: null,
+        modalOpen: false,
+      });
+    };
 
     const handleLoadMore = () => {
-      axios.get(`${this.baseUrl}/?q=${this.state.searchText}&key=${this.apiKey}&image_type=photo&orientation=horizontal&per_page=${this.state.countOnPage}&page=${this.state.page+1}`)
-    .then(res=>{
-    this.setState({
-      ...this.state,
-      images: [
-        ...this.state.images,
-        ...res.data.hits
-      ], 
-      page : this.state.page+1
-    })
-    })
-    
-    }
-    return(
-    <div>
-      <Searchbar 
-        updateSearch={updateSearch}
-        searchAPI={searchAPI}
-      />
-      {this.state.images.length !== 0 ? <ImageGallery handleOpenModal={handleOpenModal} imagesStore = {this.state.images}/> : null}
-      {this.state.images.length !== 0 ? <Button handleLoadMore = {handleLoadMore}/> : null}
-      {this.state.modalOpen && <Modal imgModal = {this.state.imgModal} handleCloseModal={handleCloseModal}/>}
-    </div>
-  )};
-};
+      axios
+        .get(
+          `${this.baseUrl}/?q=${this.state.searchText}&key=${
+            this.apiKey
+          }&image_type=photo&orientation=horizontal&per_page=${
+            this.state.countOnPage
+          }&page=${this.state.page + 1}`
+        )
+        .then(res => {
+          this.setState({
+            ...this.state,
+            images: [...this.state.images, ...res.data.hits],
+            page: this.state.page + 1,
+          });
+        });
+    };
+    return (
+      <div>
+        <Searchbar updateSearch={updateSearch} searchAPI={searchAPI} />
+        {this.state.images.length !== 0 ? (
+          <ImageGallery
+            handleOpenModal={handleOpenModal}
+            imagesStore={this.state.images}
+          />
+        ) : null}
+        {this.state.images.length !== 0 ? (
+          <Button handleLoadMore={handleLoadMore} />
+        ) : null}
+        {this.state.modalOpen && (
+          <Modal
+            imgModal={this.state.imgModal}
+            handleCloseModal={handleCloseModal}
+          />
+        )}
+      </div>
+    );
+  }
+}
--- src/components/modal/Modal.jsx
+++ src/components/modal/Modal.jsx
@@ -1,16 +1,16 @@
-import propTypes from 'prop-types'
-import s from './modal.module.css'
+import propTypes from 'prop-types';
+import s from './modal.module.css';
 
 const Modal = (handleCloseModal, imgModal) => {
-    return (
-        <div onClick={handleCloseModal} className={s.modalContainer}> 
-            <img className={s.modalImg} src={imgModal}/>
-             </div>
-    )
-}
+  return (
+    <div onClick={handleCloseModal} className={s.modalContainer}>
+      <img className={s.modalImg} src={imgModal} />
+    </div>
+  );
+};
 
 Modal.propTypes = {
-    imgModal: propTypes.string,
-    handleCloseModal: propTypes.func
-}
-export default Modal
+  imgModal: propTypes.string,
+  handleCloseModal: propTypes.func,
+};
+export default Modal;
